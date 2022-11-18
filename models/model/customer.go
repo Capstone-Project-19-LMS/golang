e369d3bb7bc2f562bcbf1eee6a4d4ec72c100d9f
AlimulDevs <alimuldev@gmail.com>
2022-11-19 03:47:06 +0700

    update

--- models/model/customer.go
+++ models/model/customer.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Customer struct {
 	gorm.Model
-	Name                 string `json:"name" gorm:"notNull;size:255"`
-	Email                string `json:"email" gorm:"notNull;unique;size:255"`
-	Password             string `json:"password" gorm:"notNull"`
-	ProfilePicture       string `json:"profile_picture" gorm:"size:255;default:null"`
+	Name           string `json:"name" gorm:"notNull;size:255"`
+	Email          string `json:"email" gorm:"notNull;unique;size:255"`
+	Password       string `json:"password" gorm:"notNull"`
+	ProfilePicture string `json:"profile_picture" gorm:"size:255;default:null"`
 }
--- repository/customerRepository/customer-myql.go
+++ repository/customerRepository/customer-myql.go
@@ -17,9 +17,10 @@ type customerRepository struct {
 // CreateCustomer implements CustomerRepository
 func (u *customerRepository) CreateCustomer(customer dto.UserRegister) error {
 	customerModel := model.Customer{
-		Name:     customer.Name,
-		Email:    customer.Email,
-		Password: customer.Password,
+		Name:           customer.Name,
+		Email:          customer.Email,
+		Password:       customer.Password,
+		ProfilePicture: "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
 	}
 	err := u.db.Create(&customerModel).Error
 	if err != nil {
