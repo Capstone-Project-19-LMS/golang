a9e9b9f5a9ec87a979fd8849a8e1ea2f1f2cac35
AlimulDevs <alimuldev@gmail.com>
2022-12-22 00:14:15 +0700

    tes add

--- service/moduleService/module_suite_test.go
+++ service/moduleService/module_suite_test.go
@@ -5,9 +5,11 @@ import (
 	"golang/models/dto"
 	moduleMockRepository "golang/repository/moduleRepository/moduleMockRepository"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
 )
 
 type suiteModule struct {
@@ -96,7 +98,7 @@ func (s *suiteModule) TestDeleteModule() {
 		ExpectedError   error
 	}{
 		{
-			"success delete assignment",
+			"success delete module",
 			dto.User{
 				ID:   "1",
 				Role: "customer",
@@ -107,7 +109,7 @@ func (s *suiteModule) TestDeleteModule() {
 			nil,
 		},
 		{
-			"fail delete assignment",
+			"fail delete module",
 			dto.User{
 				ID:   "1",
 				Role: "customer",
@@ -122,6 +124,346 @@ func (s *suiteModule) TestDeleteModule() {
 		mockCall := s.mock.On("DeleteModule", mock.Anything).Return(v.MockReturnError)
 		s.T().Run(v.Name, func(t *testing.T) {
 			err := s.moduleService.DeleteModule(v.ParamID)
+			if v.HasReturnError {
+				s.Error(err)
+				s.Equal(v.ExpectedError, err)
+				s.EqualError(err, v.ExpectedError.Error())
+			} else {
+				s.NoError(err)
+			}
+		})
+		// remove mock
+		mockCall.Unset()
+	}
+}
+
+func (s *suiteModule) TestGetModuleByID() {
+	testCase := []struct {
+		Name            string
+		ParamID         string
+		CustomerID      string
+		MockReturnBody  dto.ModuleCourseAcc
+		MockReturnError error
+		HasReturnBody   bool
+		ExpectedBody    dto.ModuleCourseAcc
+		ExpectedError   error
+	}{
+		{
+			"success get module by id ",
+			"abcde",
+			"abcde",
+			dto.ModuleCourseAcc{
+				ID:        "abcde",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				DeletedAt: gorm.DeletedAt{},
+				Name:      "tes",
+				Content:   "tes",
+				CourseID:  "abcde",
+				Course: struct {
+					Name        string "json:\"name\""
+					Description string "json:\"description\""
+					Objective   string "json:\"objective\""
+				}{},
+				MediaModules: []dto.MediaModule{
+					dto.MediaModule{
+						ID:        "abcde",
+						CreatedAt: time.Now(),
+						UpdatedAt: time.Now(),
+						DeletedAt: gorm.DeletedAt{},
+						Url:       "tes",
+						ModuleID:  "abcde",
+					},
+				},
+			},
+			nil,
+			true,
+			dto.ModuleCourseAcc{
+				ID:        "abcde",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				DeletedAt: gorm.DeletedAt{},
+				Name:      "tes",
+				Content:   "tes",
+				CourseID:  "abcde",
+				Course: struct {
+					Name        string "json:\"name\""
+					Description string "json:\"description\""
+					Objective   string "json:\"objective\""
+				}{},
+				MediaModules: []dto.MediaModule{
+					dto.MediaModule{
+						ID:        "abcde",
+						CreatedAt: time.Now(),
+						UpdatedAt: time.Now(),
+						DeletedAt: gorm.DeletedAt{},
+						Url:       "tes",
+						ModuleID:  "abcde",
+					},
+				},
+			},
+			nil,
+		},
+		{
+			"failed get module by id",
+			"abcde",
+			"abcde",
+			dto.ModuleCourseAcc{},
+			gorm.ErrRecordNotFound,
+			false,
+			dto.ModuleCourseAcc{},
+			gorm.ErrRecordNotFound,
+		},
+	}
+	for _, v := range testCase {
+		mockCall := s.mock.On("GetModuleByID", v.ParamID).Return(v.MockReturnBody, v.MockReturnError)
+		s.T().Run(v.Name, func(t *testing.T) {
+			module, err := s.moduleService.GetModuleByID(v.ParamID, v.CustomerID)
+			if v.HasReturnBody {
+				s.NoError(err)
+				s.Equal(v.ExpectedBody, module)
+			} else {
+				s.Error(err)
+				s.EqualError(err, v.ExpectedError.Error())
+				s.Equal(v.ExpectedBody, module)
+			}
+		})
+		// remove mock
+		mockCall.Unset()
+	}
+}
+
+func (s *suiteModule) TestGetAllModule() {
+	testCase := []struct {
+		Name            string
+		User            dto.User
+		MockReturnBody  []dto.Module
+		MockReturnError error
+		HasReturnBody   bool
+		ExpectedBody    []dto.Module
+		ExpectedError   error
+	}{
+		{
+			"success get all Module",
+			dto.User{
+				ID:   "abcde",
+				Role: "customer",
+			},
+			[]dto.Module{
+				{
+					ID:        "abcde",
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+					DeletedAt: gorm.DeletedAt{},
+					Name:      "tes",
+					Content:   "tes",
+					CourseID:  "abcde",
+				},
+				{
+					ID:        "abcde",
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+					DeletedAt: gorm.DeletedAt{},
+					Name:      "tes",
+					Content:   "tes",
+					CourseID:  "abcde",
+				},
+			},
+			nil,
+			true,
+			[]dto.Module{
+				{
+					ID:        "abcde",
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+					DeletedAt: gorm.DeletedAt{},
+					Name:      "tes",
+					Content:   "tes",
+					CourseID:  "abcde",
+				},
+				{
+					ID:        "abcde",
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+					DeletedAt: gorm.DeletedAt{},
+					Name:      "tes",
+					Content:   "tes",
+					CourseID:  "abcde",
+				},
+			},
+			nil,
+		},
+		{
+			"failed get all Module",
+			dto.User{
+				ID:   "abcde",
+				Role: "customer",
+			},
+			[]dto.Module{},
+			gorm.ErrRecordNotFound,
+			false,
+			nil,
+			gorm.ErrRecordNotFound,
+		},
+	}
+	for _, v := range testCase {
+		mockCall := s.mock.On("GetAllModule").Return(v.MockReturnBody, v.MockReturnError)
+		s.T().Run(v.Name, func(t *testing.T) {
+			Module, err := s.moduleService.GetAllModule()
+			if v.HasReturnBody {
+				s.NoError(err)
+				s.Equal(v.ExpectedBody, Module)
+			} else {
+				s.Error(err)
+				s.EqualError(err, v.ExpectedError.Error())
+				s.Equal(v.ExpectedBody, Module)
+			}
+		})
+		// remove mock
+		mockCall.Unset()
+	}
+}
+
+func (s *suiteModule) GetModuleByCourseID() {
+	testCase := []struct {
+		Name            string
+		CourseID        string
+		CustomerID      string
+		MockReturnBody  []dto.ModuleCourse
+		MockReturnError error
+		HasReturnBody   bool
+		ExpectedBody    []dto.ModuleCourse
+		ExpectedError   error
+	}{
+		{
+			"success get module by id ",
+			"abcde",
+			"abcde",
+			[]dto.ModuleCourse{
+				{
+					ID:        "abcde",
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+					DeletedAt: gorm.DeletedAt{},
+					Name:      "tes",
+					Content:   "tes",
+					CourseID:  "abcde",
+					Course: struct {
+						Name        string "json:\"name\""
+						Description string "json:\"description\""
+						Objective   string "json:\"objective\""
+					}{},
+					MediaModules: []dto.MediaModule{
+						{
+
+							ID:        "abcde",
+							CreatedAt: time.Now(),
+							UpdatedAt: time.Now(),
+							DeletedAt: gorm.DeletedAt{},
+							Url:       "tes",
+							ModuleID:  "abcde",
+						},
+						{
+
+							ID:        "abcde",
+							CreatedAt: time.Now(),
+							UpdatedAt: time.Now(),
+							DeletedAt: gorm.DeletedAt{},
+							Url:       "tes",
+							ModuleID:  "abcde",
+						},
+					},
+				},
+			},
+			nil,
+			true,
+			[]dto.ModuleCourse{},
+			nil,
+		},
+		{
+			"failed get module by id",
+			"abcde",
+			"abcde",
+			[]dto.ModuleCourse{},
+			gorm.ErrRecordNotFound,
+			false,
+			[]dto.ModuleCourse{},
+			gorm.ErrRecordNotFound,
+		},
+	}
+	for _, v := range testCase {
+		mockCall := s.mock.On("GetModuleByCourseID", v.CourseID).Return(v.MockReturnBody, v.MockReturnError)
+		s.T().Run(v.Name, func(t *testing.T) {
+			module, err := s.moduleService.GetModuleByCourseID(v.CourseID, v.CustomerID)
+			if v.HasReturnBody {
+				s.NoError(err)
+				s.Equal(v.ExpectedBody, module)
+			} else {
+				s.Error(err)
+				s.EqualError(err, v.ExpectedError.Error())
+				s.Equal(v.ExpectedBody, module)
+			}
+		})
+		// remove mock
+		mockCall.Unset()
+	}
+}
+
+func (s *suiteModule) TestUpdateModule() {
+	testCase := []struct {
+		Name            string
+		User            dto.User
+		ParamID         string
+		Body            dto.ModuleTransaction
+		MockReturnError error
+		HasReturnError  bool
+		ExpectedError   error
+	}{
+		{
+			"success update module",
+			dto.User{
+				ID:   "1",
+				Role: "user",
+			},
+			"abcde",
+			dto.ModuleTransaction{
+				ID:            "abcde",
+				Name:          "tes",
+				Content:       "tes",
+				CourseID:      "abcde",
+				NoModule:      1,
+				MediaModuleID: "abcde",
+				Url:           "tes",
+			},
+			nil,
+			false,
+			nil,
+		},
+		{
+			"fail update module",
+			dto.User{
+				ID:   "1",
+				Role: "user",
+			},
+			"abcde",
+			dto.ModuleTransaction{
+				ID:            "abcde",
+				Name:          "tes",
+				Content:       "tes",
+				CourseID:      "abcde",
+				NoModule:      1,
+				MediaModuleID: "abcde",
+				Url:           "tes",
+			},
+			errors.New("error"),
+			true,
+			errors.New("error"),
+		},
+	}
+	for _, v := range testCase {
+		mockCall := s.mock.On("UpdateModule", mock.Anything).Return(v.MockReturnError)
+		s.T().Run(v.Name, func(t *testing.T) {
+			err := s.moduleService.UpdateModule(v.Body)
 			if v.HasReturnError {
 				s.Error(err)
 				s.Equal(v.ExpectedError, err)
